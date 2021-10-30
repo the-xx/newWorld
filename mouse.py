34aa8129541c77f6ad2c5364aa5603a4f4342a18
Xinghai Zhang <xinghai@umich.edu>
2021-10-29 21:25:50 -0700

    add mouse coordinates

--- mouse.py
+++ mouse.py
@@ -1,10 +1,12 @@
-import win32api, win32con, time
+import win32api
+import win32con
+import time
+
 
 class Mouse:
     def __init__(self) -> None:
         self.pressed_left = False
         self.pressed_right = False
-
 
     def press_left_mouse_key(self):
         if not self.pressed_left:
@@ -16,7 +18,10 @@ class Mouse:
             win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
             self.pressed_left = False
 
-    def click_left_mouse_key(self, delay_in_milli=0.05):
+    def click_left_mouse_key(self, delay_in_milli=0.05, x=None, y=None):
+        if x and y:
+            win32api.SetCursorPos((x, y))
+            time.sleep(0.02)
         self.press_left_mouse_key()
         time.sleep(delay_in_milli)
         self.release_left_mouse_key()
@@ -31,7 +36,10 @@ class Mouse:
             win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0)
             self.pressed_right = False
 
-    def click_right_mouse_key(self, delay_in_milli=0.05):
+    def click_right_mouse_key(self, delay_in_milli=0.05, x=None, y=None):
+        if x and y:
+            win32api.SetCursorPos((x, y))
+            time.sleep(0.02)
         self.press_right_mouse_key()
         time.sleep(delay_in_milli)
         self.release_right_mouse_key()
